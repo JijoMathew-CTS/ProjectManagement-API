c0d8b403dbdbddc14e943b84bbdf3c2393f573af
Mathew <325758@cognizant.com>
2020-07-13 19:29:48 +0530

    Project management API

---
+++ src/main/java/com/fse/pm/utils/CorsConfigUtil.java
@@ -0,0 +1,17 @@
+package com.fse.pm.utils;
+
+public class CorsConfigUtil{
+ 
+//	@Bean
+//	public WebMvcConfigurer configure() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				// TODO Auto-generated method stub
+//				WebMvcConfigurer.super.addCorsMappings(registry);
+//				registry.addMapping("/*").allowedOrigins("*");
+//			}
+//		};
+//	}
+   
+}
